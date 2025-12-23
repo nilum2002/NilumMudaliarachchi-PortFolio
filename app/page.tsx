1917a3ed70f36b4bfd5d7b68bd775164f650b5ca
Nilum <sachithyanilum@gmail.com>
2025-12-23 20:09:22 +0530

    feat: add AboutMe and Achievements components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
-import Index from "./UI-Components/index";
 import Hero from "./UI-Components/index/Hero/page";
+import AboutMe from "./UI-Components/index/AboutMe/page";
+import Achievements from "./UI-Components/index/Achivements/page";
 
 export default function Home() {
   return (
     <>
-    <Hero/>
+      <Hero />
+      <AboutMe />
+      <Achievements/>
     </>
   );
 }
