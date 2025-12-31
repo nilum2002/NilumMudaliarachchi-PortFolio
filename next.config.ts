78e1dcc112e2a0204623dc2b599bb1dbaecb0cd9
Nilum <sachithyanilum@gmail.com>
2025-12-31 18:26:25 +0530

    feat: update assetPrefix in next.config.ts for correct deployment URL

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/NilumMudaliarachchi-PortFolio',
-  assetPrefix: '/NilumMudaliarachchi-PortFolio',
+  assetPrefix: 'https://nilum2002.github.io/NilumMudaliarachchi-PortFolio',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
