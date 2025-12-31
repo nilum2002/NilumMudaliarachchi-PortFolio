fa1393f56c329255ecc48342d872bb2036ae79c9
Nilum <sachithyanilum@gmail.com>
2025-12-31 18:04:33 +0530

    feat: update next.config.ts to set basePath and assetPrefix for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/NilumMudaliarachchi-PortFolio',
+  assetPrefix: '/NilumMudaliarachchi-PortFolio',
   images: {
     unoptimized: true,
   },
